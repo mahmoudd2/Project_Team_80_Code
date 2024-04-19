85faab38f461c7355265b7f980f6bd9f192cbf8a
mahmoudd2 <mahmouddahmedd220@gmail.com>
2024-04-19 20:25:41 +0200

    RR Update

--- RR.c
+++ RR.c
@@ -6,19 +6,22 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-void delay(char policy)
+void delay()
 {
-    if (policy == SCHED_RR || policy == SCHED_OTHER)
+    int secs = 1000000;
+    for (int i = 0; i < secs;i++)
     {
-        int secs = 1000000;
-        for (int i = 0; i < secs;i++)
-        {
             //msh mehtagen haga hena.
-        }
     }
 }
 
-void print_timestamp(const char* message, long thread_id) {
+typedef struct {
+    int seconds;
+    int milliseconds;
+    long microseconds;
+} Timestamp;
+
+Timestamp get_timestamp() {
     struct timeval current_time;
     gettimeofday(&current_time, NULL);
     long microsec = current_time.tv_usec;
@@ -26,113 +29,158 @@ void print_timestamp(const char* message, long thread_id) {
     struct tm *timeinfo;
     time(&rawtime);
     timeinfo = localtime(&rawtime);
-    int hours = timeinfo->tm_hour;
-    int minutes = timeinfo->tm_min;
     int seconds = timeinfo->tm_sec;
     int milliseconds = microsec / 1000;
     microsec = microsec % 1000;
 
-    printf("[%02d:%02d:%02d:%03d:%ldμs] thread_id<%ld>: %s\n", hours, minutes, seconds, milliseconds, microsec, thread_id, message);
+    // Create a Timestamp structure and populate its fields
+    Timestamp timestamp;
+    timestamp.seconds = seconds;
+    timestamp.milliseconds = milliseconds;
+    timestamp.microseconds = microsec;
+
+    // Return the Timestamp structure
+    return timestamp;
 }
 
-typedef struct {
-    int *param1;
-    char param2;
-} ThreadParams;
+Timestamp timestamp_difference(Timestamp t1, Timestamp t2) {
+    Timestamp diff;
 
-void *threadFunction(void *vargp) {
-    //int *threadId = (int *)vargp;
+    // Calculate the difference in seconds
+    diff.seconds = t2.seconds - t1.seconds;
 
-    ThreadParams *params = (ThreadParams *)vargp;
-    int *param1 = params->param1;
-    char param2 = params->param2;
+    // Calculate the difference in milliseconds
+    diff.milliseconds = t2.milliseconds - t1.milliseconds;
 
+    // Calculate the difference in microseconds
+    diff.microseconds = t2.microseconds - t1.microseconds;
+
+    // Adjust the difference if microseconds is negative
+    if (diff.microseconds < 0) {
+        diff.microseconds += 1000;
+        diff.milliseconds--;
+    }
+
+    // Adjust the difference if milliseconds is negative
+    if (diff.milliseconds < 0) {
+        diff.milliseconds += 1000;
+        diff.seconds--;
+    }
+
+    return diff;
+}
+
+void Tfunc1 (void*vargp)
+{
+    printf("Inside the first thread\n");
     pthread_t id = pthread_self();
-    print_timestamp("",id); ///hena el moshkela bta3t el params.
 
-    printf("Thread ID: %d Hello from thread!\n", param1);// w fe kol el print statments.
-    printf("Thread ID: %d Doing some work...\n", param1);
-    delay(param2);//param2 deh el policy el hnkhtrha.
-    printf("Thread ID: %d Finishing up...\n", param1);
-    printf("\n");
-    pthread_exit(NULL);
-    print_timestamp("",id);
+    Timestamp start = get_timestamp();
+    printf("Start Time : %d seconds, %d milliseconds, %ld microseconds\n", start.seconds, start.milliseconds, start.microseconds);
 
-    return NULL;
+    printf ("first thread id %lu\n", (unsigned long) id);
+    printf("Doing some work...\n");
+    delay();
+    printf("Finishing First Thread Up....\n");
+    
+    Timestamp end = get_timestamp();
+    printf("End Time: %d seconds, %d milliseconds, %ld microseconds\n", end.seconds, end.milliseconds, end.microseconds);
+    
+    Timestamp diff = timestamp_difference(start, end);
+    printf("Excution Time: %d seconds, %d milliseconds, %ld microseconds\n\n", diff.seconds, diff.milliseconds, diff.microseconds);
+
+    pthread_exit(NULL); 
 }
+void Tfunc2 (void*vargp)
+{
+    printf("Inside the second thread\n");
+    pthread_t id = pthread_self();
+
+    Timestamp start = get_timestamp();
+    printf("Start Time : %d seconds, %d milliseconds, %ld microseconds\n", start.seconds, start.milliseconds, start.microseconds);
+
+    printf ("second thread id %lu\n", (unsigned long) id);
+    printf("Doing some work...\n");
+    delay();
+    printf("Finishing Second Thread up...\n");
+    
+    Timestamp end = get_timestamp();
+    printf("End Time: %d seconds, %d milliseconds, %ld microseconds\n", end.seconds, end.milliseconds, end.microseconds);
+    
+    Timestamp diff = timestamp_difference(start, end);
+    printf("Excution Time: %d seconds, %d milliseconds, %ld microseconds\n\n", diff.seconds, diff.milliseconds, diff.microseconds);
+
+    pthread_exit(NULL); 
+}
+void Tfunc3 (void*vargp)
+{
+    printf("Inside the third thread\n");
+    pthread_t id = pthread_self();
+
+    Timestamp start = get_timestamp();
+    printf("Start Time : %d seconds, %d milliseconds, %ld microseconds\n", start.seconds, start.milliseconds, start.microseconds);
+
+    printf ("third thread id %lu\n", (unsigned long) id);
+    printf("Doing some work...\n");
+    delay();
+    printf("Finishing Third Thread up...\n");
+    
+    Timestamp end = get_timestamp();
+    printf("End Time: %d seconds, %d milliseconds, %ld microseconds\n", end.seconds, end.milliseconds, end.microseconds);
+    
+    Timestamp diff = timestamp_difference(start, end);
+    printf("Excution Time: %d seconds, %d milliseconds, %ld microseconds\n\n", diff.seconds, diff.milliseconds, diff.microseconds);
+
+    pthread_exit(NULL); 
+}
+void Tfunc4 (void*vargp)
+{
+    printf("Inside the fourth thread\n");
+    pthread_t id = pthread_self();
+
+    Timestamp start = get_timestamp();
+    printf("Start Time : %d seconds, %d milliseconds, %ld microseconds\n", start.seconds, start.milliseconds, start.microseconds);
+
+    printf ("Fourth thread id %lu\n", (unsigned long) id);
+    printf("Doing some work...\n");
+    delay();
+    printf("Finishing Fourth Thread up...\n");
+    
+    Timestamp end = get_timestamp();
+    printf("End Time: %d seconds, %d milliseconds, %ld microseconds\n", end.seconds, end.milliseconds, end.microseconds);
+    
+    Timestamp diff = timestamp_difference(start, end);
+    printf("Excution Time: %d seconds, %d milliseconds, %ld microseconds\n\n", diff.seconds, diff.milliseconds, diff.microseconds);
+
+    pthread_exit(NULL); 
+}
+
 
 int main() {
     pthread_t threads[4];
     int threadsIDS[4];
     int i;
-    //int param1 = threadsIDS[i];
     pthread_attr_t attr;
     struct sched_param param;
-    struct timespec quantum ;
-
-    #define SCHED_OTHER		0
-    #define SCHED_FIFO		1
-    #define SCHED_RR		2
-    
-    int policy;
-    char param2 = policy;
-
-    printf("Enter the desired scheduling policy:\n");
-    printf("1. SCHED_FIFO\n");
-    printf("2. SCHED_RR\n");
-    printf("3. SCHED_OTHER\n");
-    printf("Enter your choice (1-3): ");
-    scanf("%d", &policy);
 
     pthread_attr_init(&attr);
-
-    pthread_attr_setschedpolicy(&attr, policy);
-
-    switch (policy) {
-        case 1:
-            param.sched_priority=99;
-            policy = SCHED_FIFO;
-            break;
-        case 2:
-            param.sched_priority=99;
-            policy = SCHED_RR;
-            break;
-        case 3:
-            param.sched_priority = 0;
-            policy = SCHED_OTHER;
-            break;
-        default:
-            fprintf(stderr, "Incorrect Choice, Using The Default Policy RR");
-            policy = SCHED_RR;
-    }
-
-    param.sched_priority = 10; // Set priority to 10
+    pthread_attr_setschedpolicy(&attr, SCHED_RR);
+    param.sched_priority = sched_get_priority_max(SCHED_RR);
     pthread_attr_setschedparam(&attr, &param);
-  // Get the time quantum for Round Robin scheduling
-    int result = sched_rr_get_interval(0, &quantum);
-    if (result == -1) {
-        perror("sched_rr_get_interval");
-        return errno;
-    }
-
-    printf("Time quantum for Round Robin scheduling: %ld ns\n", quantum.tv_nsec);
 
 
-
-    for(i = 0; i < 4; i++) {
-        threadsIDS[i] = i;
-        int param1 = threadsIDS[i];
-        ThreadParams params;
-        params.param1 = param1;
-        params.param2 = param2;
-
-        pthread_create(&threads[i], &attr, threadFunction,&threadsIDS[i]); // na2esna el passing bta3 el parameters be el tare2a el sah. w nfham eh el far2 lma ahot el attr aw el params
-    }
+    pthread_create(&threads[1], &attr, Tfunc1,&threadsIDS[1]);
+    pthread_create(&threads[2], &attr, Tfunc2,&threadsIDS[2]); 
+    pthread_create(&threads[3], &attr, Tfunc3,&threadsIDS[3]); 
+    pthread_create(&threads[4], &attr, Tfunc4,&threadsIDS[4]); 
+    
 
     // Wait for all threads to finish
-    for(i = 0; i < 4; i++) {
-        pthread_join(threads[i], NULL);
-    }
+
+    pthread_join(threads[1], NULL);
+    pthread_join(threads[2], NULL);
+    pthread_join(threads[3], NULL);
+    pthread_join(threads[4], NULL);
 
 
     printf("Main thread: All threads finished.\n");
